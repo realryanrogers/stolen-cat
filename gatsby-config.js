f212d31374c269373729dae1a62039aae0317056
Ryan <ryansrogers85@gmail.com>
2020-07-27 11:59:22 -0400

    fixed description and title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Stolen Cat Studios`,
+    description: `Stolen Cat Game Studio`,
+    author: `@realryansrogers`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
